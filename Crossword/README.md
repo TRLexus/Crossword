fdf329d4a62ebd82dc37e86e57b2a24210fe4e2f
TRLexus <95573385+TRLexus@users.noreply.github.com>
2024-01-12 13:33:10 +0700

    Add files via upload

---
+++ Crossword/README.md
@@ -0,0 +1,3 @@
+# CrossWord
+
+A CrossWord Generator developed using HTML, CSS, BootStrap, REACT, FireBase (WIP)
---
+++ Crossword/src/Components/Header.js
@@ -0,0 +1,12 @@
+import GithubCorner from 'react-github-corner';
+
+const Header = () => {
+    return(
+        <>
+            <h3 className="text-center fw-bolder mt-4">Crossword Puzzle for CODERS!</h3>
+            <GithubCorner href="https://github.com/vpore/CrossWord" target="_blank"/>
+        </>
+    );
+}
+
+export default Header;
